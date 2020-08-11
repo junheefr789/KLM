4defbabede05de432a7d697d7176f5f1add933a4
user <user@DESKTOP-CG0OT10>
2020-08-12 02:36:25 +0900

    ev3 메세지 컨버터 수정

--- KORAC's_Learning_Machine/ev3_message_converter.py
+++ KORAC's_Learning_Machine/ev3_message_converter.py
@@ -9,47 +9,6 @@ class MessageType(enum.Enum):
     Numeric = 1
     Logic = 2
 
-def decodeMessage(s, msgType):
-    payloadSize = struct.unpack_from('<H', s, 0)[0]
-    
-    if payloadSize < 5:       # includes the mailSize
-        raise BufferError('Payload size is too small')
-    
-    a,b,c,d = struct.unpack_from('<4B', s, 2)
-    if a != 1 or b != 0 or c != 0x81 or d != 0x9e:
-        raise BufferError('Header is not correct.  Expecting 01 00 81 9e')
-    
-    mailSize = struct.unpack_from('<B', s, 6)[0]
-    
-    if payloadSize < (5 + mailSize):  # includes the valueSize
-        raise BufferError('Payload size is too small')
-    
-    mailBytes = struct.unpack_from('<' + str(mailSize) + 's', s, 7)[0]
-    mail = mailBytes.decode('ascii')[:-1]
-    
-    valueSize = struct.unpack_from('<H', s, 7 + mailSize)[0]
-    if payloadSize < (7 + mailSize + valueSize):  # includes the valueSize
-        raise BufferError('Payload size does not match the packet')
-
-    if msgType == MessageType.Logic:
-        if valueSize != 1:
-            raise BufferError('Value size is not one byte required for Logic Type')
-        valueBytes = struct.unpack_from('<B', s, 9 + mailSize)[0]
-        value = True if valueBytes != 0 else False
-    elif msgType == MessageType.Numeric:
-        if valueSize != 4:
-            raise BufferError('Value size is not four bytes required for Numeric Type')
-        value = struct.unpack_from('<f', s, 9 + mailSize)[0]
-    else:
-        valueBytes = struct.unpack_from('<' + str(valueSize) + 's', s, 9 + mailSize)[0]
-        value = valueBytes.decode('ascii')[:-1]
-    
-    remnant = None
-    if len(s) > (payloadSize + 2):
-        remnant = s[(payloadSize) + 2:]
-        
-    return (mail, value, remnant)
-
 def encodeMessage(msgType, mail, value):
     mail = mail + '\x00'
     mailBytes = mail.encode('ascii') 
